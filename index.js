f591a2f5dee10c04e5fa7751cc321afb11e284a5
johnmayor723 <36506734+johnmayor723@users.noreply.github.com>
2025-02-05 15:52:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const connectDB = require("./config/database")
 
 
 // variables
-const PORT = process.env.PORT || 3000;  // Use the port assigned by Render or default to 3000
+const PORT = 3000;  // Use the port assigned by Render or default to 3000
 const DBURL = "mongodb+srv://Pantryhubadmin:pantryhub123@cluster0.qjbxk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 // Connect to MongoDB
