8e7ed3ebabe0c41149f1045280cb0a322e58bc46
johnmayor723 <36506734+johnmayor723@users.noreply.github.com>
2025-03-12 13:18:42 +0100

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,4 @@
-oconst express = require('express');
+const express = require('express');
 const { check, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
